e827c67fa782492edfe7983edc9bdbb95b12d0cd
alexishida <alexishida@gmail.com>
2014-08-21 20:28:23 -0400

    Alteração da frequência PWM

--- CoreArduino/CoreArduino.ino
+++ CoreArduino/CoreArduino.ino
@@ -3,8 +3,8 @@
 *    Comunicação Serial para Envio e Recebimento de infomações                *
 *    pela porta Serial                                                        *
 *                                                                             *
-*    Versão: 0.0.1                                                            *
-*    Data: 17/03/2014                                                         *
+*    Versão: 0.0.2                                                            *
+*    Data: 21/08/2014                                                         *
 *    Autor: Alex Ishida                                                       *
 *    Site: http://alexishida.com                                              *
 *    E-mail: alexishida@gmail.com                                             *
@@ -19,24 +19,31 @@
 
 
 /******************************************************************/
-/* Definição de Constantes                                        */
+/* Definição de Constantes e Pinos                                */
 /******************************************************************/
 
 #define dht_dpin A15 //Pino DATA do Sensor ligado na porta Analogica A1
 
-int CANAL_UV = 12;
-int CANAL_AZUL = 11;
-int CANAL_VERDE = 10;
-int CANAL_VERMELHO = 9;
-int CANAL_BRANCO = 8;
+int canal_01 = 12;
+int canal_02 = 11;
+int canal_03 = 10;
+int canal_04 = 9;
+int canal_05 = 8;
+int canal_06 = 7;
+int canal_07 = 6;
 
-int VALOR_UV = 0;
-int VALOR_AZUL = 0;
-int VALOR_VERDE = 0;
-int VALOR_VERMELHO = 0;
-int VALOR_BRANCO = 0;
+int canal_01_valor = 0;
+int canal_02_valor = 0;
+int canal_03_valor = 0;
+int canal_04_valor = 0;
+int canal_05_valor = 0;
+int canal_06_valor = 0;
+int canal_07_valor = 0;
 
 
+/* Define o led de status */
+int led_stautus_pin = 13;
+bool led_status = false;
 
 /******************************************************************/
 /* Definição de Variáveis                                         */
@@ -49,14 +56,29 @@ void setup()
 {
   Serial.begin(9600);
   
-  pinMode(CANAL_UV, OUTPUT);
-  pinMode(CANAL_AZUL, OUTPUT);
-  pinMode(CANAL_VERDE, OUTPUT);
-  pinMode(CANAL_VERMELHO, OUTPUT);
-  pinMode(CANAL_BRANCO, OUTPUT);
+ /* 
+   Aqui eu altero a frequência dos timer 1,2,3,4  
+   Se alterar o timer 0 irá influenciar nas funções de controle de tempo do Arduino ex.: delay();
+*/   
+  TCCR1B = (TCCR1B & 0xF8) | 0x01; // 31.374 KHz
+  TCCR2B = (TCCR2B & 0xF8) | 0x01; // 31.374 KHz
+  TCCR3B = (TCCR3B & 0xF8) | 0x01; // 31.374 KHz
+  TCCR4B = (TCCR4B & 0xF8) | 0x01; // 31.374 KHz
+  
+  
+  pinMode(canal_01, OUTPUT);
+  pinMode(canal_02, OUTPUT);
+  pinMode(canal_03, OUTPUT);
+  pinMode(canal_04, OUTPUT);
+  pinMode(canal_05, OUTPUT);
+  pinMode(canal_06, OUTPUT);
+  pinMode(canal_07, OUTPUT);
+  
+  pinMode(led_stautus_pin, OUTPUT);
+
   
 /*  delay(1000);//Aguarda 1 seg antes de acessar as informações do sensor */
- //Serial.write("conectado;");
+ Serial.println("conectado;");
 }
 
 void loop()
@@ -72,34 +94,73 @@ void loop()
      else if(comando.startsWith("luzes")) {
        
       obtemLuzes(comando);
+       Serial.println("luzes;true");
+      
      }
-  
-  
+    
 
   }
+   else {
+       
+       /* Led de aguardo*/
+       ledStatus();
+       
+     }
     
   atualizaLuzes();
   
 }
 
+
+void ledStatus() {
+  delay(500);
+  if(led_status) {
+    digitalWrite(led_stautus_pin, HIGH); 
+    led_status = false;
+  } else {
+    digitalWrite(led_stautus_pin, LOW);
+    led_status = true;
+  }
+
+}
+
+
+
 void atualizaLuzes() {
   
-   analogWrite(CANAL_UV, VALOR_UV);
-   analogWrite(CANAL_AZUL, VALOR_AZUL);
-   analogWrite(CANAL_VERDE, VALOR_VERDE);
-   analogWrite(CANAL_VERMELHO, VALOR_VERMELHO);
-   analogWrite(CANAL_BRANCO, VALOR_BRANCO);
+   analogWrite(canal_01, canal_01_valor);
+   analogWrite(canal_02, canal_02_valor);
+   analogWrite(canal_03, canal_03_valor);
+   analogWrite(canal_04, canal_04_valor);
+   analogWrite(canal_05, canal_05_valor);
+   analogWrite(canal_06, canal_06_valor);
+   analogWrite(canal_07, canal_07_valor);
+
    
 }
 
+
 void obtemLuzes(String dados) {
   
-   //luzes;100;210;160;200;150
-   VALOR_UV = dados.substring(6,9).toInt();
-   VALOR_AZUL = dados.substring(10,13).toInt();
-   VALOR_VERDE = dados.substring(14,17).toInt();
-   VALOR_VERMELHO = dados.substring(18,21).toInt();
-   VALOR_BRANCO = dados.substring(22,25).toInt();
+   //luzes;100;210;160;200;150;100;50
+   canal_01_valor = dados.substring(6,9).toInt();
+   canal_02_valor = dados.substring(10,13).toInt();
+   canal_03_valor = dados.substring(14,17).toInt();
+   canal_04_valor = dados.substring(18,21).toInt();
+   canal_05_valor = dados.substring(22,25).toInt();
+   canal_06_valor = dados.substring(26,29).toInt();
+   canal_07_valor = dados.substring(30,33).toInt();
+   
+   
+   /* Debugar */
+   Serial.println(canal_01_valor);
+   Serial.println(canal_02_valor);
+   Serial.println(canal_03_valor);
+   Serial.println(canal_04_valor);
+   Serial.println(canal_05_valor);
+   Serial.println(canal_06_valor);
+   Serial.println(canal_07_valor);
+ 
    
 }
 
---
+++ Interface/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aquaino | Sistema de Automação e Controle de Aquário v1.0</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <!-- bootstrap 3.0.2 -->
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <!-- font Awesome -->
+        <link href="../assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="../assets/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <!-- Morris chart -->
+        <link href="../assets/css/morris/morris.css" rel="stylesheet" type="text/css" />
+        <!-- jvectormap -->
+        <link href="../assets/css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        <!-- Date Picker -->
+        <link href="../assets/css/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+        <!-- Daterange picker -->
+        <link href="../assets/css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+        <!-- bootstrap wysihtml5 - text editor -->
+        <link href="../assets/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+        <!-- Theme style -->
+        <link href="../assets/css/AdminLTE.css" rel="stylesheet" type="text/css" />
+        <!-- Aquaino Theme style -->
+        <link href="../assets/css/aquaino.css" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.../assets/css/1.3.0/respond.min.js"></script>
+        <![endif]-->
